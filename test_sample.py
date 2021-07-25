8523bf136eef504c08214f7ed2d1713bfd0f4c52
git-bash0528 <git.hub0528@gmail.com>
2021-07-25 21:30:48 +0530

    update

---
+++ test_sample.py
@@ -0,0 +1,16 @@
+class TestSample:
+    def test_sample1(self):
+        pass
+
+    def test_sample2(self):
+        pass
+
+    def test_sample3(self):
+        assert 5==7, 'AGAIN FAILED'
+
+    def test_sample4(self):
+        pass
+
+    def test_sample5(self):
+        assert 1==2,'FAILED!!'
+
